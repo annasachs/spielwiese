6f870bf0fb116dda99530e370b261b56cae259d7
Steffen Zimmermann <steffen.zimmermann@smail.th-koeln.de>
2020-04-18 23:28:21 +0200

    Updates

---
+++ precache-manifest.4a7fe33e07b07f289cf4316f76dc0384.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afdf6bb370fd579e2fe12df928382c78",
+    "url": "/spielwiese/index.html"
+  },
+  {
+    "revision": "bc1a4eec571ba01235c2",
+    "url": "/spielwiese/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ee0e0fe1b47eab34a103",
+    "url": "/spielwiese/static/js/2.b84e8ac7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spielwiese/static/js/2.b84e8ac7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc1a4eec571ba01235c2",
+    "url": "/spielwiese/static/js/main.e0437eb8.chunk.js"
+  },
+  {
+    "revision": "5eabbedd2b6a022aefda",
+    "url": "/spielwiese/static/js/runtime-main.5fe27f7c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spielwiese/static/media/logo.5d5d9eef.svg"
+  }
+]);
